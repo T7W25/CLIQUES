b45db8b7ae5f7a9e0a8e415c446555b2cdca5bcb
Lucky-chaudhary <lucky.handles11@gmail.com>
2025-03-18 19:10:15 -0400

    port change
    Port for mac

--- back-end/server.js
+++ back-end/server.js
@@ -17,6 +17,6 @@ app.get("/api/test", (req, res) => {
 
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
  
