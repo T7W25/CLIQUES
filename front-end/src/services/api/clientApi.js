47426b209e4be210725d62fb36f7930a67bf4ef9
Saurav <saurav.chaudhary3355@gmail.com>
2025-03-23 01:40:58 -0400

    client api
    updated api for client - booking services

--- front-end/src/services/api/clientApi.js
+++ front-end/src/services/api/clientApi.js
@@ -1,1 +1,12 @@
- 
+import axios from "axios";
+
+const SERVICE_URL = "/api/service";
+const BOOKING_URL = "/api/booking";
+
+export const fetchApprovedServices = () => axios.get(`${SERVICE_URL}/approved`);
+
+export const bookService = (data) =>
+  axios.post(`${BOOKING_URL}/create`, data, {
+    headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+  });
+
