4f3dfd19c4d3c2b07e19882c4b14db56c1eb8b59
Guilherme Passos <guilherme2005.passos@gmail.com>
2025-03-23 01:34:05 -0400

    Update Booking.js

--- back-end/models/Booking.js
+++ back-end/models/Booking.js
@@ -1,1 +1,14 @@
- 
+const mongoose = require("mongoose");
+
+const bookingSchema = new mongoose.Schema({
+  clientId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  providerId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  serviceId: { type: mongoose.Schema.Types.ObjectId, ref: "Service", required: true },
+  date: { type: String, required: true },
+  time: { type: String, required: true },
+  location: { type: String, required: true },
+  status: { type: String, default: "Pending" },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Booking", bookingSchema); 
