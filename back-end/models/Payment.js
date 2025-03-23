2446841c5e6f6534bb275622ca6d315152339d78
liu301318128 <qliu103@my.centennialcollege.ca>
2025-03-23 19:12:15 -0400

    update payment.js

--- back-end/models/Payment.js
+++ back-end/models/Payment.js
@@ -3,9 +3,11 @@ const mongoose = require("mongoose");
 const paymentSchema = new mongoose.Schema({
   bookingId: { type: mongoose.Schema.Types.ObjectId, ref: "Booking", required: true },
   clientId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  providerId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // ADDED
   amount: { type: Number, required: true },
   status: { type: String, enum: ["Pending", "Completed", "Failed"], default: "Pending" },
   paymentDate: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model("Payment", paymentSchema);
+// payment
+module.exports = mongoose.model("Payment", paymentSchema);
