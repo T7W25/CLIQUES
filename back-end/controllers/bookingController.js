463413e3c4da12fd825c7b6798027539a4dfd247
Yujuan Cui <ycui31@my.centennialcollege.ca>
2025-03-23 01:36:46 -0400

    updated booking controller.js

--- back-end/controllers/bookingController.js
+++ back-end/controllers/bookingController.js
@@ -1,1 +1,21 @@
- 
+const Booking = require("../models/Booking");
+
+exports.createBooking = async (req, res) => {
+  const { serviceId, providerId, date, time, location } = req.body;
+
+  try {
+    const booking = new Booking({
+      clientId: req.user.id,
+      providerId,
+      serviceId,
+      date,
+      time,
+      location,
+    });
+
+    await booking.save();
+    res.status(201).json({ message: "Booking successful!", booking });
+  } catch (err) {
+    res.status(500).json({ message: "Booking failed", error: err.message });
+  }
+};
