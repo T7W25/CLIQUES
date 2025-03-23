0dae37df621ffb120c8598cc4d840d56bbe3f93e
Saurav <saurav.chaudhary3355@gmail.com>
2025-03-23 01:14:27 -0400

    provider api
    route for provider api

--- front-end/src/services/api/providerApi.js
+++ front-end/src/services/api/providerApi.js
@@ -1,1 +1,9 @@
+import axios from "axios";
+
+const API_URL = "/api/service";
+
+export const createService = (data) =>
+  axios.post(`${API_URL}/create`, data, {
+    headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+  });
  
