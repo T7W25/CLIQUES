095e78ea55fe2b618fe491c9ccf7c79717e2eeb2
cgaldona <cgaldona@my.centennialcollege.ca>
2025-03-23 01:37:59 -0400

    Update BookingRoutes.js

--- back-end/routes/bookingRoutes.js
+++ back-end/routes/bookingRoutes.js
@@ -1,1 +1,9 @@
- 
+const express = require("express");
+const { createBooking } = require("../controllers/bookingController");
+const authMiddleware = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+router.post("/create", authMiddleware, createBooking);
+
+module.exports = router;
