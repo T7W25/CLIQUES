a2ed1c16dad02dcb263bc4ff7e7dc3295f039075
kajal patel <kajolpatel28@gmail.com>
2025-03-23 18:02:04 -0400

    correct error

--- back-end/routes/bookingRoutes.js
+++ back-end/routes/bookingRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { createBooking } = require("../controllers/bookingController");
+const { createBooking, getClientBookings } = require("../controllers/bookingController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/create", authMiddleware, createBooking);
+router.get("/client", authMiddleware, getClientBookings); // User Story 6
 
 module.exports = router;
