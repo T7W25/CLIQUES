d147ed09f34e48b36b84e22ddcd1cb5829bd4ada
kajal patel <kajolpatel28@gmail.com>
2025-03-23 18:22:58 -0400

    userroutes changes

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -1,3 +1,4 @@
+// user routes
 const express = require("express");
 const { getProfile, updateProfile } = require("../controllers/userController");
 const authMiddleware = require("../middleware/authMiddleware");
