8e54fbf7cf580503de302926c5e479719e71f4d0
Guilherme Passos <guilherme2005.passos@gmail.com>
2025-03-23 01:50:14 -0400

    Update Payment.js

--- back-end/models/Payment.js
+++ back-end/models/Payment.js
@@ -1,1 +1,11 @@
- 
+const mongoose = require("mongoose");
+
+const paymentSchema = new mongoose.Schema({
+  bookingId: { type: mongoose.Schema.Types.ObjectId, ref: "Booking", required: true },
+  clientId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  amount: { type: Number, required: true },
+  status: { type: String, enum: ["Pending", "Completed", "Failed"], default: "Pending" },
+  paymentDate: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Payment", paymentSchema);
