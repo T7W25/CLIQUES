367a07b0003103c2b0f99c0459ff56cc9f4f87a7
Guilherme Passos <guilherme2005.passos@gmail.com>
2025-03-23 01:07:52 -0400

    Database for Service.js

--- back-end/models/Service.js
+++ back-end/models/Service.js
@@ -1,1 +1,14 @@
- 
+const mongoose = require("mongoose");
+
+const serviceSchema = new mongoose.Schema({
+  providerId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  title: { type: String, required: true },
+  description: { type: String },
+  category: { type: String },
+  price: { type: Number, required: true },
+  availability: { type: String },
+  isApproved: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Service", serviceSchema);
