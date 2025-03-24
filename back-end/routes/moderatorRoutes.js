03811873ad876022a90e3eb4c9c88a6834f7aa89
cgaldona <cgaldona@my.centennialcollege.ca>
2025-03-23 23:50:50 -0400

    Update moderator.js

--- back-end/routes/moderatorRoutes.js
+++ back-end/routes/moderatorRoutes.js
@@ -1,1 +1,9 @@
- 
+const express = require("express");
+const { getAllReports, updateReportStatus } = require("../controllers/moderatorController");
+
+const router = express.Router();
+
+router.get("/reports", getAllReports);
+router.put("/reports/:id/status", updateReportStatus);
+
+module.exports = router;
