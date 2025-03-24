84b1fc50e1b4c0736e9f06da25e40d6c388285ac
Guilherme Passos <guilherme2005.passos@gmail.com>
2025-03-24 00:18:32 -0400

    Update Service.js

--- back-end/models/Service.js
+++ back-end/models/Service.js
@@ -3,12 +3,14 @@ const mongoose = require("mongoose");
 const serviceSchema = new mongoose.Schema({
   providerId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   title: { type: String, required: true },
+  name: { type: String }, 
   description: { type: String },
   category: { type: String },
   price: { type: Number, required: true },
   availability: { type: String },
+  status: { type: String, default: "Pending" }, 
   isApproved: { type: Boolean, default: false },
-  createdAt: { type: Date, default: Date.now },
+  createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model("Service", serviceSchema);
