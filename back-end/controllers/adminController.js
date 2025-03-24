6b6a0b5f68dabfd076f4261836bf8c57a3a2375a
Yujuan Cui <ycui31@my.centennialcollege.ca>
2025-03-24 00:28:56 -0400

    updated admin  controller.js

--- back-end/controllers/adminController.js
+++ back-end/controllers/adminController.js
@@ -1,1 +1,22 @@
  
+const User = require("../models/User");
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({}, "name role email");
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch users" });
+  }
+};
+
+exports.updateUserRole = async (req, res) => {
+  const { id } = req.params;
+  const { role } = req.body;
+  try {
+    const user = await User.findByIdAndUpdate(id, { role }, { new: true });
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to update user role" });
+  }
+};
