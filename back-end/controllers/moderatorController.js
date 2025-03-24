8f5bc99405f626471a0d058f768497f7e4814fd3
Yujuan Cui <ycui31@my.centennialcollege.ca>
2025-03-23 23:42:45 -0400

    updated moderate

--- back-end/controllers/moderatorController.js
+++ back-end/controllers/moderatorController.js
@@ -1,1 +1,22 @@
  
+const Report = require("../models/Report");
+
+exports.getAllReports = async (req, res) => {
+  try {
+    const reports = await Report.find();
+    res.json(reports);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch reports" });
+  }
+};
+
+exports.updateReportStatus = async (req, res) => {
+  const { id } = req.params;
+  const { status } = req.body;
+  try {
+    const report = await Report.findByIdAndUpdate(id, { status }, { new: true });
+    res.json(report);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to update status" });
+  }
+};
