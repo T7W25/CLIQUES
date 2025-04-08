b312750ecd667e408e09a0f592e37bf3196b13bd
kajal patel <kajolpatel28@gmail.com>
2025-04-07 22:46:56 -0400

    client api

--- front-end/src/services/api/clientApi.js
+++ front-end/src/services/api/clientApi.js
@@ -1,1 +1,16 @@
  
+import axios from "axios";
+
+export const getOrders = async () => {
+  const res = await axios.get(${process.env.REACT_APP_API_URL}/api/client/orders, {
+    headers: { Authorization: Bearer ${localStorage.getItem("token")} }
+  });
+  return res.data;
+};
+
+export const submitFeedback = async (data) => {
+  const res = await axios.post(${process.env.REACT_APP_API_URL}/api/client/feedback, data, {
+    headers: { Authorization: Bearer ${localStorage.getItem("token")} }
+  });
+  return res.data;
+};
