810981cfbc32a96353acab4ebd704720e2cccedb
cgaldona <cgaldona@my.centennialcollege.ca>
2025-04-07 22:44:10 -0400

    Update clientRoutes.js

--- back-end/routes/clientRoutes.js
+++ back-end/routes/clientRoutes.js
@@ -1,1 +1,9 @@
- 
+const express = require("express");
+const { getClientOrders, submitFeedback } = require("../controllers/clientController");
+const router = express.Router();
+const authMiddleware = require("../middleware/authMiddleware");
+
+router.get("/orders", authMiddleware, getClientOrders);
+router.post("/feedback", authMiddleware, submitFeedback);
+
+module.exports = router;
