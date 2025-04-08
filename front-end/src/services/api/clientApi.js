e9103ea487a63d2200a8c62133c870b261fa1348
kajal patel <kajolpatel28@gmail.com>
2025-04-07 23:11:05 -0400

    added more api to client api

--- front-end/src/services/api/clientApi.js
+++ front-end/src/services/api/clientApi.js
@@ -14,3 +14,22 @@ export const submitFeedback = async (data) => {
   });
   return res.data;
 };
+
+export const getClientBookings = async () => {
+    const res = await axios.get(${process.env.REACT_APP_API_URL}/api/client/orders, {
+      headers: { Authorization: Bearer ${localStorage.getItem("token")} },
+    });
+    return res.data;
+  };
+  
+  export const cancelBooking = async (id) => {
+    return await axios.put(${process.env.REACT_APP_API_URL}/api/client/cancel/${id}, {}, {
+      headers: { Authorization: Bearer ${localStorage.getItem("token")} },
+    });
+  };
+  
+  export const rescheduleBooking = async (id, newDate) => {
+    return await axios.put(${process.env.REACT_APP_API_URL}/api/client/reschedule/${id}, { newDate }, {
+      headers: { Authorization: Bearer ${localStorage.getItem("token")} },
+    });
+  };
