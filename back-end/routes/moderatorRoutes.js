13963df9bb3152736c514e66c6ff3aba6f5be5a7
cgaldona <cgaldona@my.centennialcollege.ca>
2025-04-09 12:04:53 -0400

    Update moderatorRoutes.js

--- back-end/routes/moderatorRoutes.js
+++ back-end/routes/moderatorRoutes.js
@@ -1,1 +1,9 @@
- 
+const express = require("express");
+const router = express.Router();
+const { getUnresolvedReports, escalateToAdmin } = require("../controllers/moderatorController");
+const auth = require("../middleware/authMiddleware");
+
+router.get("/reports", auth, getUnresolvedReports);
+router.put("/escalate/:id", auth, escalateToAdmin);
+
+module.exports = router;
