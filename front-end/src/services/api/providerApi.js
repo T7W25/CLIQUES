576891a69ce326e5d3228d6f0f72d6a2e8bd4579
kajal patel <kajolpatel28@gmail.com>
2025-04-08 23:59:46 -0400

    created providerapi.js

--- front-end/src/services/api/providerApi.js
+++ front-end/src/services/api/providerApi.js
@@ -1,1 +1,18 @@
  
+import axios from "axios";
+
+const API = ${process.env.REACT_APP_API_URL}/api/services;
+
+export const getMyServices = async () => {
+  const res = await axios.get(${API}/my-services, {
+    headers: { Authorization: Bearer ${localStorage.getItem("token")} }
+  });
+  return res.data;
+};
+
+export const updatePromotionalPrice = async (id, data) => {
+  const res = await axios.put(${API}/promo/${id}, data, {
+    headers: { Authorization: Bearer ${localStorage.getItem("token")} }
+  });
+  return res.data;
+};
