16c3679edb874984e5acb25a5331fc35eaf201d9
cgaldona <cgaldona@my.centennialcollege.ca>
2025-04-09 11:48:31 -0400

    Update providerRoutes.js

--- back-end/routes/providerRoutes.js
+++ back-end/routes/providerRoutes.js
@@ -1,1 +1,9 @@
- 
+const express = require("express");
+const { getProviderAnalytics } = require("../controllers/providerController");
+const auth = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+router.get("/analytics", auth, getProviderAnalytics);
+
+module.exports = router;
