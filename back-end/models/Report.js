7c15643bded9396957970f606aaaba33d5f0fc0c
Deepamjindal2004 <jindaldeepam@gmail.com>
2025-04-09 12:01:01 -0400

    Update Report.js

--- back-end/models/Report.js
+++ back-end/models/Report.js
@@ -1,1 +1,16 @@
- 
+const mongoose = require("mongoose");
+
+const reportSchema = new mongoose.Schema({
+  issue: String,
+  status: {
+    type: String,
+    enum: ["pending", "in_review", "resolved", "escalated"],
+    default: "pending"
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Report", reportSchema);
