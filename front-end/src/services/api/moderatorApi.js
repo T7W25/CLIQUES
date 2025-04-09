74f749b9cc6f9674f74880448082ce6d09b1ced9
kajal patel <kajolpatel28@gmail.com>
2025-04-09 11:59:47 -0400

    create modertor api,js

--- front-end/src/services/api/moderatorApi.js
+++ front-end/src/services/api/moderatorApi.js
@@ -1,1 +1,18 @@
  
+import axios from "axios";
+
+const API = ${process.env.REACT_APP_API_URL}/api/moderator;
+
+export const getReports = async () => {
+  const res = await axios.get(${API}/reports, {
+    headers: { Authorization: Bearer ${localStorage.getItem("token")} }
+  });
+  return res.data;
+};
+
+export const escalateReport = async (id) => {
+  const res = await axios.put(${API}/escalate/${id}, {}, {
+    headers: { Authorization: Bearer ${localStorage.getItem("token")} }
+  });
+  return res.data;
+};
