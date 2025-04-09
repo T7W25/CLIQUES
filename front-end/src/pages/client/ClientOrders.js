d0ad7881876864bb37518769ece19b73e30fd1e0
Lucky-chaudhary <lucky.handles11@gmail.com>
2025-04-08 23:17:41 -0400

    Created ClientOrder
    Updated ClientOrder.js

---
+++ front-end/src/pages/client/ClientOrders.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import { getOrders } from "../../services/api/clientApi";
+import OrderFeedbackForm from "../../components/client/OrderFeedbackForm";
+
+const ClientOrders = () => {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      const res = await getOrders();
+      if (res.success) setOrders(res.orders);
+    };
+    fetchOrders();
+  }, []);
+
+  return (
+    <div>
+      <h2>My Orders</h2>
+      {orders.map(order => (
+        <div key={order._id} className="order-card">
+          <p>Service: {order.serviceName}</p>
+          <p>Date: {order.date}</p>
+          <OrderFeedbackForm bookingId={order._id} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ClientOrders;
