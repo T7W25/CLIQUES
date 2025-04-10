55269b41b5c3324c75476b6e7b8a2f1bea56ec2d
cgaldona <cgaldona@my.centennialcollege.ca>
2025-04-09 22:59:31 -0400

    Update adminRoutes.js

--- back-end/routes/adminRoutes.js
+++ back-end/routes/adminRoutes.js
@@ -1,1 +1,14 @@
- 
+const express = require("express");
+const router = express.Router();
+const {
+  getAllUsers,
+  toggleUserStatus,
+  updateUserRole
+} = require("../controllers/adminController");
+const auth = require("../middleware/authMiddleware");
+
+router.get("/users", auth, getAllUsers);
+router.put("/users/:id/toggle-status", auth, toggleUserStatus);
+router.put("/users/:id/role", auth, updateUserRole);
+
+module.exports = router;
