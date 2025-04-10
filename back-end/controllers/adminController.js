8907928f2d79f9ca2cf5bc90e5454b5dc91f614f
Yujuan Cui <ycui31@my.centennialcollege.ca>
2025-04-09 22:56:20 -0400

    updated admin controller.js

--- back-end/controllers/adminController.js
+++ back-end/controllers/adminController.js
@@ -1,1 +1,23 @@
  
+const User = require("../models/User");
+
+exports.toggleUserStatus = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    user.suspended = !user.suspended;
+    await user.save();
+    res.json({ success: true, user });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Toggle status failed" });
+  }
+};
+
+exports.updateUserRole = async (req, res) => {
+  try {
+    const { role } = req.body;
+    const user = await User.findByIdAndUpdate(req.params.id, { role }, { new: true });
+    res.json({ success: true, user });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Role update failed" });
+  }
+};
